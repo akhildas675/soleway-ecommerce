51297a5e32887bd63d54c616ac0977fac668b027
AKHILDAS - H <akhildas675@gmail.com>
2025-08-26 18:03:59 +0530

    images added

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const passport=require('passport')
 require('./passport')
 
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/soleway', {
 }).then(() => {
     console.log('Connected to MongoDB');
 }).catch((error) => {
