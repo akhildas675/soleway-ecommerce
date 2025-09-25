e69d8f66c785b1643a996decf15b3fb308576465
AKHILDAS - H <akhildas675@gmail.com>
2025-09-25 17:07:16 +0530

    online payment and wallet payment handlers set

---
+++ helper/services/paymentHanlders/walletPaymentHandler.js
@@ -0,0 +1,71 @@
+const Wallet = require('../../../Model/walletModel');
+const createSingleOrder = require('../orderServices');
+const {updateProductStock}= require('../../utils/productUtils');
+const {clearCart} = require('../../utils/cartUtils')
+
+
+
+const handleWalletPayment = async (data) => {
+  const { findCart, findAddress, userId, totalAmount, finalAmount, discountAmount, couponDetails, paymentMethod } = data;
+  
+  // Check wallet balance
+  let wallet = await Wallet.findOne({ userId });
+  if (!wallet) {
+    wallet = await Wallet.create({ userId, balance: 0 });
+  }
+
+  if (wallet.balance < finalAmount) {
+    return {
+      success: false,
+      message: "Insufficient wallet balance",
+      statusCode: 400
+    };
+  }
+
+  const orderIds = [];
+
+  
+  for (let item of findCart.cartProducts) {
+    const product = item.productId;
+    const productPrice = product.offerPrice || product.realPrice;
+    const productTotal = productPrice * item.quantity;
+    const productDiscount = couponDetails.couponCode ? (productTotal / totalAmount) * discountAmount : 0;
+    const finalProductTotal = productTotal - productDiscount;
+
+    // Create order
+    const orderId = await createSingleOrder({
+      item,
+      findAddress,
+      userId,
+      finalProductTotal,
+      couponDetails,
+      paymentMethod,
+      orderStatus: "Order Placed",
+      paymentStatus: "Received"
+    });
+
+    orderIds.push(orderId);
+
+
+    wallet.balance -= finalProductTotal;
+    wallet.history.push({
+      amount: finalProductTotal,
+      status: "Debit",
+      transactionDate: new Date(),
+    });
+
+   
+    await updateProductStock(product._id, item.size, item.quantity);
+  }
+
+  await wallet.save();
+  await clearCart(userId);
+
+  return {
+    success: true,
+    message: "Order placed successfully and wallet amount deducted",
+    orderIds
+  };
+};
+
+module.exports = { handleWalletPayment };
