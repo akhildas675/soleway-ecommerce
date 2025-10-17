1cd734ebfa9189eda8e74c2d60076d104597bcfa
Akhildas - H <akhildas675@gmail.com>
2025-10-17 14:42:16 +0530

    passport  url updated

--- passport.js
+++ passport.js
@@ -10,10 +10,6 @@ passport.deserializeUser((user, done) => {
     done(null, user);
 });
 
-// console.log('CLIENT_ID', process.env.CLIENT_ID);  // Should print your Google Client ID
-// console.log('CLIENT_SECRET', process.env.CLIENT_SECRET);  // Should print your Google Client Secret
-
-
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
@@ -23,4 +19,3 @@ passport.use(new GoogleStrategy({
 function(request, accessToken, refreshToken, profile, done) {
     return done(null, profile);
 }));
-
